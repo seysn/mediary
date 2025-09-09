72b7dd45e9c140178166f97b084801e36de837a9
Nicolas SEYS <seys.nicolas@gmail.com>
2025-09-09 16:57:39 +0200

    Init mediary-cli crate

---
+++ crates/mediary-cli/src/main.rs
@@ -0,0 +1,70 @@
+use std::{ffi::OsStr, fs::File, path::PathBuf, str::FromStr};
+
+use clap::Parser;
+use mediary_jpeg::RawJpeg;
+use mediary_netpbm::NetpbmImage;
+
+#[derive(Debug, Parser)]
+#[clap(version)]
+struct Args {
+    from: FromImage,
+    to: ToImage,
+}
+
+#[derive(Debug, Clone)]
+enum FromImage {
+    Jpeg(PathBuf),
+}
+
+#[derive(Debug, Clone)]
+enum ToImage {
+    Netpbm(PathBuf),
+}
+
+impl FromStr for FromImage {
+    type Err = std::convert::Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let path: PathBuf = s.parse()?;
+
+        match path.extension().and_then(OsStr::to_str) {
+            Some("jpeg") | Some("jpg") => Ok(Self::Jpeg(path)),
+            ext => unimplemented!("Cannot decode extension {ext:?}"),
+        }
+    }
+}
+
+impl FromStr for ToImage {
+    type Err = std::convert::Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let path: PathBuf = s.parse()?;
+
+        match path.extension().and_then(OsStr::to_str) {
+            Some("pbm") | Some("pgm") | Some("ppm") => Ok(Self::Netpbm(path)),
+            ext => unimplemented!("Cannot decode extension {ext:?}"),
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
+    let args = Args::parse();
+
+    let image = match args.from {
+        FromImage::Jpeg(path) => {
+            let jpeg = RawJpeg::read(path)?;
+            jpeg.decode()
+        }
+    };
+
+    match args.to {
+        ToImage::Netpbm(path) => {
+            let output = File::create(path)?;
+            NetpbmImage::new(image).write(output)?;
+        }
+    }
+
+    Ok(())
+}
